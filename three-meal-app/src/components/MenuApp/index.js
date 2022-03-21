b826da9cef78dd7ac5fab80679a0e7a9e71ce0f2
nicholas-wahba <nicholaswahba@knights.ucf.edu>
2022-03-21 18:47:08 -0400

    main

--- three-meal-app/src/components/MenuApp/index.js
+++
@@ -1,9 +0,0 @@
-import Menu from "../Menu";
-
-export default function MenuApp({ data }){
-return(
-  <div className = "App">
-  {data.map((item,index) => <Menu {...item} key={index}></Menu>)}
-  </div>
-  )
-}
